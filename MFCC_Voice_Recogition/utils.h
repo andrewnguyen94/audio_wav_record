8ab5cc0a088ec5b2ba2039cf6d98a88aabfba66f
Your Name <anhtuck01@gmail.com>
2018-08-10 13:19:17 +0700

    DFT temporary

--- MFCC_Voice_Recogition/utils.h
+++ MFCC_Voice_Recogition/utils.h
@@ -4,15 +4,30 @@
 /*includes
 ------------------------------------------
 ------------------------------------------*/
-
+#include "record.h"
+#include <math.h>
 
 /*defines, constants
 ------------------------------------------
 ------------------------------------------*/
+struct SIGNAL {
+	SAMPLE *raw_signal;
+	const float frame_length;		//so sample trong 1 frame
+	const float step_lengh;		//do dai bc nhay
+	const int num_frame;			//so frame trong 1 audio signal
+};
+
+struct COMPLEX {
+	float img;
+	float real;
+	float magnitude;	
+};
+
+#define PI 3.14159265359
 
 /*functions
 ------------------------------------------
 ------------------------------------------*/
-//float *dct_signals(float *input);
-
+SAMPLE *DFT(SAMPLE *signal);
+float HammingWindow(float a);
 #endif
