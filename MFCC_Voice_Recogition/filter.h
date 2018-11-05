be10d76c6d82f0e3ec3187f3cdb8856977b076fc
andrewnguyen94 <anhtrungnguyen94@gmail.com>
2018-11-05 14:01:28 +0700

    abcxyz

--- MFCC_Voice_Recogition/filter.h
+++ MFCC_Voice_Recogition/filter.h
@@ -17,4 +17,7 @@
 float *butterworth_bandpass(int order, float *signal, int size, float sample_rate, float high_freq_cutoff, float low_freq_cutoff);
 float *butterworth_lowpass(int order, float *signal, int size, float sample_rate, float cutoff_freq);
 
+float *butterworth_bandpass_v2(int order, float *signal, int size, float sample_rate, float high_freq_cutoff, float low_freq_cutoff, float *A,
+	float *d1, float *d2, float *d3, float *d4, float *w0, float *w1, float *w2, float *w3, float *w4, float *x);
+
 #endif // !FILTER_H
